edbc856fc750d9ffc5fe996ef33fc3f00a3ae844
Anurag Negi <negianurag707@gmail.com>
2025-04-05 07:12:13 +0530

    updates

--- db/db.js
+++ db/db.js
@@ -6,6 +6,7 @@ const connect=db.createPool({
     user:process.env.DB_USERNAME || 'root',
     password:process.env.DB_PASSWORD || 'root123',
     database:process.env.DB_NAME || 'bedev',
+    port:process.db.env.DB_PORT ||  3306,
     waitForConnections: true,
     connectionLimit:10,
     queueLimit:0,
